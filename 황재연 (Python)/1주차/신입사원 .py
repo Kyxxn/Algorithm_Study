0f217b542f4293616a7f6d437a62384019f6ce19
jaeyeon12 <ghkdwodus1234@naver.com>
2024-01-16 20:40:51 +0900

    []

---
+++ 황재연 (Python)/1주차/신입사원 .py
@@ -0,0 +1,21 @@
+import sys
+
+input = sys.stdin.readline
+
+cases = int(input().strip())
+for _ in range(cases):
+    n = int(input().strip())
+    lose = 0
+
+    board = [list(map(int,input().split())) for __ in range(n)]
+    board.sort(key=lambda x: (x[1],x[0]))
+    
+    x = n
+
+    for i, val in enumerate(board):
+        if val[0] <= x:
+            x = val[0]
+        else:
+            lose += 1
+
+    print(n - (lose))
