4acf3596ba9c574774a63c66bde4eb4680f0677d
Kyxxn <gywns626@naver.com>
2024-01-20 21:28:33 +0900

    [ADD] boj_1613 try

--- 박효준 (Swift)/BOJ/BOJ/main.swift
+++ 박효준 (Swift)/BOJ/BOJ/main.swift
@@ -1,98 +1,52 @@
-//  24년 겨울 알고리즘 스터디
-//  BOJ & 프로그래머스
+// 24년 겨울 알고리즘 스터디
+// BOJ & 프로그래머스
 //
-//  Created by 박효준 on 1/10/24.
+// Created by 박효준 on 1/10/24.
+
+// 1. 인풋으로 값이 들어오면 양방향으로 연결
 
 import Foundation
 
-func isPrime(_ N : Int) -> Bool{
-    // 소수는 1과 자기 자신만 약수인 경우
-    // 아래 두 if 문이 없으면 95% 런타임 에러남 -> 왜 ?
-    if N <= 1 { return false }
-    if N <= 3 { return true }
-    
-    var isCheck : Bool = true
-    for i in 2...Int(sqrt(Double(N))){
-        if N % i == 0{
-            isCheck = false
-            break
+var input : [Int] = readLine()!.split(separator: " ").map{Int($0)!}
+var n : Int = input[0]
+var k : Int = input[1]
+
+var arr : [[(Int, Bool)]] = Array(repeating: [(Int, Bool)](), count: k+1)
+
+for _ in 0..<k {
+    input = readLine()!.split(separator: " ").map{Int($0)!}
+    let x = input[0]
+    let y = input[1]
+    arr[x].append((y, true))
+    arr[y].append((x, false))
+}
+
+var s : Int = Int(readLine()!)!
+for _ in 0..<s {
+    input = readLine()!.split(separator: " ").map{Int($0)!}
+    let key : Int = input[0]
+    var check : Bool = false
+    var dir : Bool = true
+    for j in 0..<arr[key].count {
+        if arr[key][j].0 == input[1] {
+            check = true
+            dir = arr[key][j].1
         }
     }
     
-    return isCheck
-}
-
-func isPalindrome(_ N : Int) -> Bool{
-    let s = String(N)
-    return s == String(s.reversed())
-}
-
-if var N = Int(readLine()!) {
-    while (true){
-        if (isPrime(N) && isPalindrome(N)) {
-            print(N)
-            break
+    if check {
+        if dir {
+            print(-1)
+        }else {
+            print(1)
         }
-        N += 1
+    }else{
+        print(0)
     }
 }
 
 
-//var n : Int = Int(readLine()!)!
-//var graph : [[(Int, Int)]] = Array(repeating: [(Int, Int)](), count: n+1)
-//var visited : [Bool] = Array(repeating: false, count: n+1)
-//
-//for _ in 0..<n-1{ // 0~10 11번
-//    let tmp : [Int] = readLine()!.split(separator: " ").map{Int($0)!}
-//    let x : Int = tmp[0]
-//    let y : Int = tmp[1]
-//    let weight : Int = tmp[2]
-//    graph[x].append((y, weight))
-//    graph[y].append((x, weight))
+//for i in 0..<k {
+//    print(arr[i])
 //}
-//// 인접 리스트 완성
-//
-//var weightSum : Int = 0 // 노드 - 노드간의 가중치
-//var rootToleaf : [Int] = Array(repeating: 0, count : n+1)
-//// 시작 노드 X에서 N개의 노드들까지 가중치를 담는 배열
-//
-//func dfs(_ vertex : Int) {
-//    visited[vertex] = true
-//    
-//    for next in graph[vertex]{
-//        let nextVertex : Int = next.0
-//        let nextWeight : Int = next.1
-//        
-//        if (!visited[nextVertex]){
-//            weightSum = weightSum + nextWeight
-//            dfs(nextVertex)
-//            weightSum = weightSum - nextWeight
-//        }else{
-//            rootToleaf[vertex] = weightSum
-//        }
-//    }
-//}
-//
-//dfs(1)
-//// 1번 : 루트와 가장 먼 리프노드
-//var weightMax : Int = rootToleaf.max()!
-//var start = rootToleaf.firstIndex(of: weightMax)!
-//
-//// 2번 : 1번 노드와 가장 먼 리프노드
-//weightSum = 0
-//for i in 0...n{
-//    visited[i] = false
-//    rootToleaf[i] = 0
-//}
-//
-//dfs(start)
-//weightMax = rootToleaf.max()!
-//print(weightMax)
-//
-////for i in rootToleaf{
-////    print(i, terminator: " ")
-////}
-//
-////for i in 0..<n{
-////    print(graph[i])
-////}
+
