b464e10f6cef25098d0e4842f371749f2ad65097
Seoung Youl <paek678@naver.com>
2024-01-23 01:36:26 +0900

    abc

---
+++ 백성열 (java)/프로그래머스_추억 점수.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Solution {
+    static int test;
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        int[] answer = new int[photo.length];
+        HashMap<String,Integer> map = new HashMap<String,Integer>();
+        for(int i = 0 ;i< yearning.length;i++){
+            map.put(name[i],yearning[i]);
+        }
+        for(int i = 0;i<photo.length;i++){
+            int result = 0;
+            for(int j = 0;j<photo[i].length;j++){
+                if(map.get(photo[i][j]) != null){
+                    result += map.get(photo[i][j]);
+                }
+            }
+            answer[i] = result;
+        }
+        return answer;
+
+        
+    }
+}
