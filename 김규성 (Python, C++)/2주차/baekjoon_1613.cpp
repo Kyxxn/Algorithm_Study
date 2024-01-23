037e78b0791ccf7740aaefccbc00faa2d1f8f6da
ChoHyeon <yunnk29@naver.com>
2024-01-23 18:47:19 +0900

    [UPDATE] 한글 오류 수정

--- 김규성 (Python, C++)/2주차/baekjoon_1613.cpp
+++ 김규성 (Python, C++)/2주차/baekjoon_1613.cpp
@@ -25,7 +25,7 @@ int main() {
         graph[v][u] = -1;
     }
 
-    // ÇÃ·ÎÀÌµå-¿ö¼È(Floyd Warshall) ¾Ë°í¸®Áò
+    // Floyd Warshall ì•Œê³ ë¦¬ì¦˜
     for (int i = 1; i <= n; ++i) {
         for (int j = 1; j <= n; ++j) {
             for (int k = 1; k <= n; ++k) {
