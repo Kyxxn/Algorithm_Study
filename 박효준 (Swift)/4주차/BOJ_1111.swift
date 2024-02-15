3e0f638667654861597972eb09fef37b6b4d0bf7
Kyxxn <gywns626@naver.com>
2024-02-15 13:22:06 +0900

    [ADD] 6주차 문제 추가

--- 박효준 (Swift)/4주차/BOJ_1111.swift
+++ 박효준 (Swift)/4주차/BOJ_1111.swift
@@ -2,50 +2,43 @@
 // BOJ & 프로그래머스
 //
 // Created by 박효준 on 1/10/24.
-// 구현 & 정렬 & 그리디
+// 구현 & BFS
 
 import Foundation
 
-var input = readLine()!.split(separator: " ").map{Int(String($0))!}
-let N = input[0], L = input[1]
-var puddle: [(Int, Int)] = []
-for _ in 0..<N {
-    input = readLine()!.split(separator: " ").map{Int(String($0))!}
-    puddle.append((input[0], input[1]))
+var puyo: [[Character]] = []
+for i in 0..<12 {
+    // 필드 정보 초기화
+    let input = readLine()!
+    let row = Array(input)
+    puyo.append(row)
 }
 
-puddle.sort { $0.0 < $1.0 }
-// (1, 6), (8, 12), (13, 17)
-
-var count = 0
-var board_idx = 0
-
-for i in 0..<puddle.count {
-    board_idx = max(puddle[i].0, board_idx)
-    // 1
+func BFS(_ i: Int, _ j: Int) {
+    var visited: [[Bool]] = Array(repeating: Array(repeating: false, count: 6+1), count: 12+1)
+    var queue: [(Int, Int)] = []
+    let mx = [0,0,-1,1]
+    let my = [-1,1,0,0]
+    visited[i][j] = true
+    queue.append((i,j))
     
-    while (puddle[i].1 - board_idx > 0) {
-        board_idx += L
-        count += 1
-    }
-}
-
-print(count)
-/* 첫 번째 풀이
-var road : [Bool] = Array(repeating: true,count: 1000000001)
-
-for _ in 0..<N {
-    let puddle = readLine()!.split(separator: " ").map{Int(String($0))!}
-    for j in puddle[0]..<puddle[1] {
-        road[j] = false
-    }
-}
-
-for i in 0..<road.count {
-    if !road[i] {
-        for j in i..<i+L {
-            road[j] = true
-            count += 1
+    while !queue.isEmpty {
+        let tmp = queue.removeFirst()
+        for next in 0..<4 {
+            let curChar = puyo[i][j]
+            let nextX = tmp.0 + mx[next]
+            let nextY = tmp.1 + my[next]
+            if !visited[nextX][nextY] && nextX > 0 && nextX < 13 && nextY > 0 && nextY < 7 {
+                if puyo[nextX][nextY] == curChar {
+                    
+                }
+            }
         }
     }
-} */
+}
+
+for i in 0..<12 {
+    for j in 0..<6 {
+        BFS(i,j)
+    }
+}
--- 박효준 (Swift)/BOJ/BOJ/main.swift
+++ 박효준 (Swift)/BOJ/BOJ/main.swift
@@ -1,38 +1,44 @@
+// 24년 겨울 알고리즘 스터디
+// BOJ & 프로그래머스
+//
+// Created by 박효준 on 1/10/24.
+
 import Foundation
 
-protocol ChildEssential {
-    func forParent()
-    func beOld()
+let nm = readLine()!.split(separator: " ").map{Int($0)!}
+let n = nm[0], m = nm[1]
+var miro: [[Int]] = Array(repeating: [], count: n)
+let mx = [0,0,-1,1], my = [1,-1,0,0]
+
+for i in 0..<n {
+    let input = readLine()!.map{Int(String($0))!}
+    miro[i] = input
 }
-class Person {
-    var name: String
-    var age: Int {
-        willSet{
-            print("나이가 \(age)에서 \(newValue)로 바뀔 예정임")
-        }
+
+var visited: [[Bool]] = Array(repeating: Array(repeating: false, count: m), count: n)
+var distance: [[Int]] = Array(repeating: Array(repeating: 0, count: m), count: n)
+func bfs() {
+    var queue: [(Int, Int)] = []
+    queue.append((0,0))
+    visited[0][0] = true
+    distance[0][0] = 1
+    
+    while !queue.isEmpty {
+        let cur = queue.removeFirst()
         
-        didSet {
-            print("ㅊㅊ \(age)됐음 원래는 \(oldValue)였음")
+        for i in 0..<4 {
+            let nx = mx[i] + cur.0, ny = my[i] + cur.1
+            
+            if nx >= 0 && nx < n && ny >= 0 && ny < m {
+                if !visited[nx][ny] && (miro[nx][ny] == 1){
+                    distance[nx][ny] = distance[cur.0][cur.1] + 1
+                    queue.append((nx, ny))
+                    visited[nx][ny] = true
+                }
+            }
         }
     }
-    
-    
-    init(name: String, age: Int) {
-        self.name = name
-        self.age = age
-    }
 }
 
-class Student: Person, ChildEssential {
-    func forParent() {
-        print("효")
-    }
-    
-    func beOld() {
-        self.age += 1
-        print("ㅊㅋㅊㅋ")
-    }
-}
-
-var student: Student = Student(name: "효준", age: 10)
-student.beOld()
+bfs()
+print(distance[n-1][m-1])
